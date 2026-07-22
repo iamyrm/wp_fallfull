a8be65837015bcb4c3c25628b549e4e44354362a
iamyrm <majhiyagya123@gmail.com>
2026-07-22 22:07:59 +0545

    content word color changing feature dynamic done, working on it

--- themes/fallfull/parts/home/section-product-desc.php
+++ themes/fallfull/parts/home/section-product-desc.php
@@ -1,12 +1,17 @@
 <?php
+global $fallfull_options;
+$word = $fallfull_options['home-prod-desc-text-color'];
+$highlight_words = $word ? array_map('trim', explode(',', $word)) : array();
+
 $heading = get_field('product_heading', get_the_ID());
 $prod_desc = get_field('product_description', get_the_ID());
+
 ?>
 
 <div class="row">
 	<div class="col-lg-8 offset-lg-2 text-center">
 		<div class="section-title">
-			<h3><?php echo fallfull_highlight_words(wp_kses_post($heading), array('Our')); ?></h3>
+			<h3><?php echo fallfull_highlight_words(wp_kses_post($heading), $highlight_words); ?></h3>
 			<p><?php echo esc_html($prod_desc); ?></p>
 		</div>
 	</div>
