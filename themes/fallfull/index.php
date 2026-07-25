a5f8e1eb0db56f9f9802a18828b5c87072459851
iamyrm <majhiyagya123@gmail.com>
2026-07-25 19:39:27 +0545

    new theme baner image added, other code added for page and index

--- themes/fallfull/index.php
+++ themes/fallfull/index.php
@@ -1,8 +1,48 @@
-<?php
-get_header();
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-get_template_part( 'entry' );
-comments_template();
-endwhile; endif;
-get_template_part( 'nav', 'below' );
-get_footer();
+<?php get_header(); ?>
+
+<!-- home page slider -->
+<?php echo get_template_part('parts/home/section', 'hero'); ?>
+<!-- end home page slider -->
+
+<!-- features list section -->
+<?php echo get_template_part('parts/home/section', 'features'); ?>
+<!-- end features list section -->
+
+<!-- product section -->
+<div class="product-section mt-150 mb-150">
+	<div class="container">
+
+		<?php
+		get_template_part('parts/home/section', 'product-desc');
+		get_template_part('parts/home/section', 'products');
+		?>
+
+	</div>
+</div>
+<!-- end product section -->
+
+<!-- deals section -->
+<?php echo get_template_part('parts/home/section', 'deals'); ?>
+<!-- end deals section -->
+
+<!-- testimonail-section -->
+<?php echo get_template_part('parts/global/section', 'testimonial'); ?>
+<!-- end testimonail-section -->
+
+<!-- advertisement section -->
+<?php echo get_template_part('parts/home/section', 'ad'); ?>
+<!-- end advertisement section -->
+
+<!-- shop sale -->
+<?php echo get_template_part('parts/global/section', 'shop-sale'); ?>
+<!-- end shop sale -->
+
+<!-- latest news -->
+<?php echo get_template_part('parts/home/section', 'news'); ?>
+<!-- end latest news -->
+
+<!-- logo carousel -->
+<?php echo get_template_part('parts/global/section', 'carousel'); ?>
+<!-- end logo carousel -->
+
+<?php get_footer(); ?>
--- themes/fallfull/page.php
+++ themes/fallfull/page.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<div class="entry-content" itemprop="mainContentOfPage">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
+
+<!-- breadcrumb-section -->
+<?php get_template_part('parts/global/breadcrumb'); ?>
+<!-- end breadcrumb section -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 offset-lg-2 text-center">
+			<div class="section-title">
+				<h3><?php the_title() ?></h3>
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
 </div>
-</article>
-<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
