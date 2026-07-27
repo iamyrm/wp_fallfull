262ce3bb7353b02f9b7968003d270c7cf84fa2ca
iamyrm <majhiyagya123@gmail.com>
2026-07-27 06:12:51 +0545

    Shop page dynamic done

--- themes/fallfull/woocommerce/loop/add-to-cart.php
+++ themes/fallfull/woocommerce/loop/add-to-cart.php
@@ -27,11 +27,11 @@ $aria_describedby = isset($args['aria-describedby_text']) ? sprintf('aria-descri
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" %s data-quantity="%s" class="%s" %s>%s</a>',
+		'<a href="%s" %s data-quantity="%s" class="fallfull-cart-btn %s" %s><i class="fas fa-shopping-cart"></i> %s</a>',
 		esc_url($product->add_to_cart_url()),
 		$aria_describedby,
 		esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
-		esc_attr(isset($args['class']) ? $args['class'] : 'button'),
+		esc_attr(isset($args['class']) ? $args['class'] : 'cart-btn'),
 		isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
 		esc_html($product->add_to_cart_text())
 	),
