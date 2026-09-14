f6afb7f11edb500c2b2a245e9fe31473614dc0a8
iamyrm <majhiyagya123@gmail.com>
2026-09-14 13:56:40 +0545

    Display only three related products in product details page

--- themes/fallfull/inc/woocommerce_func.php
+++ themes/fallfull/inc/woocommerce_func.php
@@ -156,3 +156,12 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 	}
 	add_action('woocommerce_single_product_summary', 'fallfull_prod_details_excerpt', 20);
 }
+
+// Display 3 related products in product details page
+function fallfull_display_three_related_proucts($args)
+{
+	$args['posts_per_page'] = 3;
+	$args['columns']        = 3;
+	return $args;
+}
+add_filter('woocommerce_output_related_products_args', 'fallfull_display_three_related_proucts', 9999);
